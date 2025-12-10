d1df440aa82ae6e79ad008d0c83dac3a673156e9
Krishanu Mandal <krishanu178@gmail.com>
2025-12-10 23:03:52 +0530

    Add: React Router

---
+++ ReactRouter/src/components/User/User.jsx
@@ -0,0 +1,11 @@
+function User () {
+    return (
+        <div className="mx-auto w-full max-w-7xl">
+            <h1 className="text-3xl font-bold underline">
+                User Page
+            </h1>
+        </div>
+    )
+}
+
+export default User;
